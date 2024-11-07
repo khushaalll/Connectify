ab8bb4784d974edb66be7f853e086d9380a81387
Khushal Nirmal Jain <khushalnirmal.jain@mail.concordia.ca>
2024-11-07 13:36:35 -0500

    Adding files

---
+++ client/client/client.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <thread>
+#include <string>
+#include<unordered_map>
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+class ChatClient {
+private:
+    SOCKET clientSocket;
+    string username;
+    unordered_map<int, string> activeClients; // Map of active clients with their IDs
+
+    bool Initialize() {
+        WSADATA data;
+        return WSAStartup(MAKEWORD(2, 2), &data) == 0;
+    }
+
+    void SendMessage() {
+        cout << "Enter '/msg <client_id> <message>' for private message or just <message> for a broadcast message or 'quit' to exit." << endl;
+        while (true) {
+            string message;
+            getline(cin, message);
+
+            if (message == "quit") {
+                cout << "Disconnecting from chat..." << endl;
+                break;
+            }
+
+            int bytesSent = send(clientSocket, message.c_str(), message.length(), 0);
+            if (bytesSent == SOCKET_ERROR) {
+                cout << "Failed to send message." << endl;
+                break;
+            }
+        }
+
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+    void ReceiveMessage() {
+        char buffer[4096];
+        while (true) {
+            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                cout << "Disconnected from server." << endl;
+                break;
+            }
+
+            string message(buffer, bytesReceived);
+            cout << message << endl;
+
+            // If the message contains a list of active clients
+            if (message.find("Active clients:") == 0) {
+                activeClients.clear();
+                size_t pos = message.find("\n", 0);
+                while (pos != string::npos) {
+                    size_t nextPos = message.find("\n", pos + 1);
+                    string clientInfo = message.substr(pos + 1, nextPos - pos - 1);
+                    size_t spacePos = clientInfo.find(":");
+                    if (spacePos != string::npos) {
+                        int clientId = stoi(clientInfo.substr(0, spacePos));
+                        string clientName = clientInfo.substr(spacePos + 2);
+                        activeClients[clientId] = clientName;
+                    }
+                    pos = nextPos;
+                }
+            }
+        }
+
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+public:
+    ChatClient(const string& user) : clientSocket(INVALID_SOCKET), username(user) {}
+
+    bool Connect(const string& serverAddress, int port) {
+        if (!Initialize()) {
+            cout << "Failed to initialize Winsock." << endl;
+            return false;
+        }
+
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == INVALID_SOCKET) {
+            cout << "Failed to create socket." << endl;
+            WSACleanup();
+            return false;
+        }
+
+        sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(port);
+        inet_pton(AF_INET, serverAddress.c_str(), &serverAddr.sin_addr);
+
+        if (connect(clientSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+            cout << "Failed to connect to server." << endl;
+            closesocket(clientSocket);
+            WSACleanup();
+            return false;
+        }
+
+        // Send the client name
+        int bytesSent = send(clientSocket, username.c_str(), username.length(), 0);
+        if (bytesSent == SOCKET_ERROR) {
+            cout << "Failed to send client name." << endl;
+            closesocket(clientSocket);
+            WSACleanup();
+            return false;
+        }
+
+        cout << "Connected to server." << endl;
+
+        thread sender(&ChatClient::SendMessage, this);
+        thread receiver(&ChatClient::ReceiveMessage, this);
+
+        sender.join();
+        receiver.join();
+
+        return true;
+    }
+};
+
+int main() {
+    cout << "Enter your username: ";
+    string username;
+    getline(cin, username);
+
+    ChatClient client(username);
+    if (!client.Connect("127.0.0.1", 12345)) {
+        cout << "Failed to connect to server." << endl;
+    }
+    return 0;
+}
---
+++ server/server/server.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <thread>
+#include <string>
+#include <unordered_map>
+#include <mutex>
+
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+class ChatServer {
+private:
+    SOCKET listenSocket;
+    unordered_map<int, SOCKET> clients; // Map client ID to their socket
+    unordered_map<int, string> clientNames; // Map client ID to their name
+    int nextClientId = 1;
+    mutex clientsMutex;
+
+    bool Initialize() {
+        WSADATA data;
+        return WSAStartup(MAKEWORD(2, 2), &data) == 0;
+    }
+
+    void SendMessageToAll(const string& message, SOCKET excludeSocket) {
+        lock_guard<mutex> lock(clientsMutex);
+        for (const auto& [id, clientSocket] : clients) {
+            if (clientSocket != excludeSocket) {
+                send(clientSocket, message.c_str(), message.length(), 0);
+            }
+        }
+    }
+
+    void SendClientList(SOCKET clientSocket) {
+        lock_guard<mutex> lock(clientsMutex);
+        string clientList = "Active clients:\n";
+        for (const auto& [id, clientName] : clientNames) {
+            clientList += to_string(id) + ": " + clientName + "\n";
+        }
+        send(clientSocket, clientList.c_str(), clientList.length(), 0);
+    }
+
+    void SendPrivateMessage(SOCKET senderSocket, int targetClientId, const string& message) {
+        lock_guard<mutex> lock(clientsMutex);
+        auto it = clients.find(targetClientId);
+        if (it != clients.end()) {
+            send(it->second, message.c_str(), message.length(), 0);
+        } else {
+            string errorMsg = "Error: Client " + to_string(targetClientId) + " not found.";
+            send(senderSocket, errorMsg.c_str(), errorMsg.length(), 0);
+        }
+    }
+
+    void HandleClient(int clientId, SOCKET clientSocket, const string& clientName) {
+        // Add to clients map
+        {
+            lock_guard<mutex> lock(clientsMutex);
+            clients[clientId] = clientSocket;
+            clientNames[clientId] = clientName;
+        }
+
+        // Send the client list after they connect
+        SendClientList(clientSocket);
+
+        char buffer[4096];
+        while (true) {
+            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                cout << "Client " << clientId << " disconnected." << endl;
+                break;
+            }
+
+            string message(buffer, bytesReceived);
+            cout << "Message from Client " << clientId << ": " << message << endl;
+
+            if (message.find("/msg") == 0) {
+                // Private message command: "/msg targetClientId message"
+                size_t spacePos = message.find(' ', 5);
+                if (spacePos != string::npos) {
+                    int targetClientId = stoi(message.substr(5, spacePos - 5));
+                    string privateMessage = "Private from " + to_string(clientId) + ": " + message.substr(spacePos + 1);
+                    SendPrivateMessage(clientSocket, targetClientId, privateMessage);
+                }
+            } else {
+                string broadcastMessage = "Client " + to_string(clientId) + ": " + message;
+                SendMessageToAll(broadcastMessage, clientSocket);
+            }
+        }
+
+        // Cleanup on disconnect
+        closesocket(clientSocket);
+        {
+            lock_guard<mutex> lock(clientsMutex);
+            clients.erase(clientId);
+            clientNames.erase(clientId);
+        }
+    }
+
+public:
+    ChatServer() : listenSocket(INVALID_SOCKET) {}
+
+    bool Start(int port) {
+        if (!Initialize()) {
+            cout << "Failed to initialize Winsock." << endl;
+            return false;
+        }
+
+        listenSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (listenSocket == INVALID_SOCKET) {
+            cout << "Failed to create socket." << endl;
+            WSACleanup();
+            return false;
+        }
+
+        sockaddr_in serverAddr = {0};
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(port);
+        serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+        if (bind(listenSocket, reinterpret_cast<sockaddr*>(&serverAddr), sizeof(serverAddr)) == SOCKET_ERROR) {
+            cout << "Bind failed." << endl;
+            closesocket(listenSocket);
+            WSACleanup();
+            return false;
+        }
+
+        if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
+            cout << "Listen failed." << endl;
+            closesocket(listenSocket);
+            WSACleanup();
+            return false;
+        }
+
+        cout << "Server started on port " << port << "." << endl;
+
+        while (true) {
+            SOCKET clientSocket = accept(listenSocket, nullptr, nullptr);
+            if (clientSocket == INVALID_SOCKET) {
+                cout << "Invalid client socket." << endl;
+                continue;
+            }
+
+            // Receive client name
+            char buffer[256];
+            int bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+            if (bytesReceived <= 0) {
+                cout << "Failed to receive client name." << endl;
+                closesocket(clientSocket);
+                continue;
+            }
+
+            string clientName(buffer, bytesReceived);
+            int clientId = nextClientId++;
+
+            cout << "Client " << clientName << " connected with ID " << clientId << "." << endl;
+
+            thread(&ChatServer::HandleClient, this, clientId, clientSocket, clientName).detach();
+        }
+
+        closesocket(listenSocket);
+        WSACleanup();
+        return true;
+    }
+};
+
+int main() {
+    ChatServer server;
+    if (!server.Start(12345)) {
+        cout << "Failed to start the server." << endl;
+    }
+    return 0;
+}
